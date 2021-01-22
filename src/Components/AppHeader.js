87158289e6699fc7e393695eec32a8c76f5e37c7
Nicole Dow <nikki.dow71@gmail.com>
2021-01-22 08:53:06 -0700

    for netlify deploy

--- src/Components/AppHeader.js
+++ src/Components/AppHeader.js
@@ -7,7 +7,7 @@ export default function AppHeader() {
       <Typography variant="h1" color="primary">
         Bowling Bonanza
       </Typography>
-      <p>Enter the number of pins you knock down on each try. Enter "x" or "X" for a strike.</p>
+      <p>Enter the number of pins you knock down on each try. Enter "X" for a strike.</p>
       <br/>
       <p>An empty input will be considered "0."</p>
       <br/>
